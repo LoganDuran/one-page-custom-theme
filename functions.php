00b8bc00e7ed934de2e266b37fb9b8f28777c852
LoganDuran <106428943+LoganDuran@users.noreply.github.com>
2024-11-30 14:32:15 +0100

    Add translation loading functionality in functions.php

--- functions.php
+++ functions.php
@@ -11,3 +11,12 @@ add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
 if (is_user_logged_in()) {
     add_filter('show_admin_bar', '__return_true');
 }
+
+
+
+//  Load translations
+function belikenow_load_theme_textdomain()
+{
+    load_theme_textdomain('belikenow-custom-theme', get_template_directory() . '/languages');
+}
+add_action('after_setup_theme', 'belikenow_load_theme_textdomain');
